giantswarm/gsctl

commit 6e54f40429272f6feeff009f5956ecc17b175ce1

Simplified code by removing else blocks

commands/ping.go
@@ -29,15 +29,13 @@ func ping(cmd *cobra.Command, args []string) {
 		fmt.Println(color.RedString("API cannot be reached"))
 		fmt.Println(err.Error())
 		os.Exit(1)
-	} else {
-		defer resp.Body.Close()
-		elapsed := time.Since(start)
-		if resp.StatusCode == 200 {
-			fmt.Println(color.GreenString("API connection is fine"))
-			fmt.Printf("Ping took %v\n", elapsed)
-		} else {
-			fmt.Println(color.RedString("API returned unexpected response status %v", resp.StatusCode))
-			os.Exit(2)
-		}
 	}
+	defer resp.Body.Close()
+	elapsed := time.Since(start)
+	if resp.StatusCode == 200 {
+		fmt.Println(color.GreenString("API connection is fine"))
+		fmt.Printf("Ping took %v\n", elapsed)
+	}
+	fmt.Println(color.RedString("API returned unexpected response status %v", resp.StatusCode))
+	os.Exit(2)
 }

commit 11d4925d6d7cf895e7890545378ab1fddd6ab2ea

Fixing a control flow bug I introduced
in 6e54f40429272f6feeff009f5956ecc17b175ce1

commands/ping.go
@@ -39,7 +39,8 @@ func ping(cmd *cobra.Command, args []string) {
 	if resp.StatusCode == 200 {
 		fmt.Println(color.GreenString("API connection is fine"))
 		fmt.Printf("Ping took %v\n", elapsed)
+	} else {
+		fmt.Println(color.RedString("API returned unexpected response status %v", resp.StatusCode))
+		os.Exit(2)
 	}
-	fmt.Println(color.RedString("API returned unexpected response status %v", resp.StatusCode))
-	os.Exit(2)
 }