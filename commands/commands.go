6815ff658b247793168ead81ac1f8271e3d07ba7
marians <marian@giantswarm.io>
2016-11-25 16:23:05 +0100

    Providing request ID and command line for headers
    These will be available to all commands:
    - `requestIDHeader`: a random request ID for one call
    - `cmdLine`: the command line used for this call

--- commands/commands.go
+++ commands/commands.go
@@ -2,6 +2,13 @@ package commands
 
 // This file defines some variables to be available in all commands
 
+import (
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
+
 var (
 	// token flag
 	cmdToken string
@@ -17,4 +24,33 @@ var (
 
 	// TTL (time to live) flag
 	cmdTTLDays int
+
+	randomStringCharset = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+	requestIDHeader string
+	cmdLine         string
 )
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+	requestIDHeader = randomRequestID()
+	cmdLine = getCommandLine()
+}
+
+// randomRequestID returns a new request ID
+func randomRequestID() string {
+	size := 14
+	b := make([]rune, size)
+	for i := range b {
+		b[i] = randomStringCharset[rand.Intn(len(randomStringCharset))]
+	}
+	return string(b)
+}
+
+// getCommandLine returns the command line that has been called
+func getCommandLine() string {
+	if os.Getenv("GSCTL_DISABLE_CMDLINE_TRACKING") == "" {
+		return strings.Join(os.Args, " ")
+	}
+	return ""
+}
