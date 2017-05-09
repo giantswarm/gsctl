bd5f9738b349aff6f146ea033d4e573715a5a5ae
marians <marian@giantswarm.io>
2017-05-09 10:29:18 +0200

    Relying on httptest instead of external service

--- client/client_test.go
+++ client/client_test.go
@@ -1,14 +1,25 @@
 package client
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"strings"
 	"testing"
 	"time"
 )
 
 func TestTimeout(t *testing.T) {
+	// Our test server.
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// enforce a timeout longer than the client's
+		time.Sleep(3 * time.Second)
+		fmt.Fprintln(w, "Hello")
+	}))
+	defer ts.Close()
+
 	clientConfig := Configuration{
-		Endpoint: "http://httpbin.org/delay/2?",
+		Endpoint: ts.URL,
 		Timeout:  1 * time.Second,
 	}
 	apiClient := NewClient(clientConfig)
@@ -19,8 +30,17 @@ func TestTimeout(t *testing.T) {
 }
 
 func TestUserAgent(t *testing.T) {
+	// Our test server.
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Return valid JSON containing user agent string received
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(`{"user-agent": "` + r.Header.Get("User-Agent") + `"}`))
+	}))
+	defer ts.Close()
+
 	clientConfig := Configuration{
-		Endpoint:  "https://httpbin.org/user-agent?",
+		Endpoint:  ts.URL,
 		UserAgent: "my own user agent/1.0",
 	}
 	apiClient := NewClient(clientConfig)
