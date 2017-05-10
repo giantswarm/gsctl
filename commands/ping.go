3ab7c3205fcb68f3dc2a2a36a5271400c5894759
Marian Steinbach <marian@giantswarm.io>
2017-05-10 14:10:08 +0200

    Ping refactoring, testing, sending user-agent header (#54)
    * Ping refactoring, sending user-agent header
    
    * Modified check
    
    The former version didn't work on Travis, as there the error was `Get
    http://not.existing/v1/ping: dial tcp: lookup not.existing on
    169.254.169.254:53: no such host`
    
    * ping: Change error wording

--- commands/ping.go
+++ commands/ping.go
@@ -9,6 +9,8 @@ import (
 
 	"github.com/fatih/color"
 	"github.com/spf13/cobra"
+
+	"github.com/giantswarm/gsctl/config"
 )
 
 var (
@@ -17,7 +19,7 @@ var (
 		Use:   "ping",
 		Short: "Check API connection",
 		Long:  `Tests the connection to the API`,
-		Run:   ping,
+		Run:   runPingCommand,
 	}
 )
 
@@ -25,33 +27,57 @@ func init() {
 	RootCommand.AddCommand(PingCommand)
 }
 
-// ping checks the API connections
-func ping(cmd *cobra.Command, args []string) {
-	u, err := url.Parse(cmdAPIEndpoint)
+// runPingCommand executes the ping() function
+// and prints output in a user-friendly way
+func runPingCommand(cmd *cobra.Command, args []string) {
+	duration, err := ping(cmdAPIEndpoint)
 	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Println(color.RedString("Could not reach API"))
+		fmt.Println(err)
 		os.Exit(1)
+	}
+	fmt.Println(color.GreenString("API connection is fine"))
+	fmt.Printf("Ping took %v\n", duration)
+}
+
+// ping checks the API connection and returns
+// duration (in case of success) and error (in case of failure)
+func ping(endpointURL string) (time.Duration, error) {
+	var duration time.Duration
+
+	// create URI
+	u, err := url.Parse(endpointURL)
+	if err != nil {
+		return duration, err
 	}
 	u, err = u.Parse("/v1/ping")
 	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
+		return duration, err
+	}
+
+	// create client and request
+	request, err := http.NewRequest("GET", u.String(), nil)
+	if err != nil {
+		return duration, err
+	}
+	request.Header.Set("User-Agent", config.UserAgent())
+
+	// create client
+	pingClient := &http.Client{
+		Timeout: 5 * time.Second,
 	}
 
 	start := time.Now()
-	resp, err := http.Get(u.String())
+	resp, err := pingClient.Do(request)
 	if err != nil {
-		fmt.Println(color.RedString("API cannot be reached"))
-		fmt.Println(err.Error())
-		os.Exit(1)
+		return duration, err
 	}
 	defer resp.Body.Close()
-	elapsed := time.Since(start)
-	if resp.StatusCode == 200 {
-		fmt.Println(color.GreenString("API connection is fine"))
-		fmt.Printf("Ping took %v\n", elapsed)
-	} else {
-		fmt.Println(color.RedString("API returned unexpected response status %v", resp.StatusCode))
-		os.Exit(2)
+
+	duration = time.Since(start)
+	if resp.StatusCode != http.StatusOK {
+		return duration, fmt.Errorf("bad status code %d", resp.StatusCode)
 	}
+
+	return duration, nil
 }
