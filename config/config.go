556abe60f05901c3bf473108f7c60822d17b7496
Marian Steinbach <marian@giantswarm.io>
2017-05-26 12:30:49 +0200

    gsctl: Ensure that certificate paths get updated with config dir migration (#63)
    * Rewrite certificate paths in kubectl config
    * Test case for config migration added

--- config/config.go
+++ config/config.go
@@ -281,5 +281,28 @@ func migrateConfigDir() error {
 	// ensure ~/.config exists
 	os.MkdirAll(path.Dir(DefaultConfigDirPath), 0700)
 
-	return os.Rename(LegacyConfigDirPath, DefaultConfigDirPath)
+	err = os.Rename(LegacyConfigDirPath, DefaultConfigDirPath)
+	if err != nil {
+		return err
+	}
+
+	// adapt certificate paths in kubeconfig
+	if len(KubeConfigPaths) > 0 {
+		// we only adapt the first file found (on purpose)
+		theKubeConfigPath := KubeConfigPaths[0]
+		if stat, err := os.Stat(theKubeConfigPath); err == nil {
+			oldConfig, configErr := ioutil.ReadFile(theKubeConfigPath)
+			if configErr != nil {
+				return configErr
+			}
+			newConfig := strings.Replace(string(oldConfig), LegacyConfigDirPath, DefaultConfigDirPath, -1)
+			// write back
+			writeErr := ioutil.WriteFile(theKubeConfigPath, []byte(newConfig), stat.Mode())
+			if writeErr != nil {
+				return errors.New("could not overwrite kubectl config file")
+			}
+		}
+	}
+
+	return nil
 }
