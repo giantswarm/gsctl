368e6e93b81e07713eda3cc5b7a8ef7acb08b749
Marian Steinbach <marian@giantswarm.io>
2017-06-06 18:51:33 +0200

    Adding parsing for alternative format

--- util/datetime.go
+++ util/datetime.go
@@ -14,12 +14,24 @@ const (
 func ParseDate(dateString string) time.Time {
 	// our standard format
 	template := "2006-01-02T15:04:05.000Z"
+	alternativeTemplate := "2006-01-02T15:04:05.000-07:00"
 
 	// normalizing the number of decimal places to 3
-	re := regexp.MustCompile("\\.[0-9]+Z$")
-	dateString = re.ReplaceAllLiteralString(dateString, ".000Z")
+	// and discarding sub-second detail along the way
+	re := regexp.MustCompile("\\.[0-9]+")
+	dateString = re.ReplaceAllLiteralString(dateString, ".000")
 
-	t, _ := time.Parse(template, dateString)
+	// try parsing with several formats
+	t, err := time.Parse(template, dateString)
+	if err == nil {
+		return t
+	}
+
+	t, err = time.Parse(alternativeTemplate, dateString)
+	if err == nil {
+		l, _ := time.LoadLocation("UTC")
+		t = t.In(l)
+	}
 	return t
 }
 
