7a254f0a8f255178f75618a78e42e1cbc2d0de5e
Marian Steinbach <marian@giantswarm.io>
2017-06-06 18:25:29 +0200

    Adding a test case

--- util/datetime_test.go
+++ util/datetime_test.go
@@ -13,13 +13,22 @@ type Test struct {
 
 var tests = []Test{
 	{
+		// input string
 		"2006-01-02T15:04:05.000Z",
+		// time value
 		time.Date(2006, time.January, 2, 15, 4, 5, 0, time.UTC),
-		"2006 Jan 02, 15:04"},
+		// output string
+		"2006 Jan 02, 15:04 UTC",
+	},
 	{
 		"1999-11-24T00:57:28.999999Z",
 		time.Date(1999, time.November, 24, 0, 57, 28, 0, time.UTC),
-		"1999 Nov 24, 00:57",
+		"1999 Nov 24, 00:57 UTC",
+	},
+	{
+		"2016-12-05T15:41:46.965138231+01:00",
+		time.Date(2016, time.December, 5, 14, 41, 46, 0, time.UTC),
+		"2016 Dec 05, 14:41 UTC",
 	},
 }
 
