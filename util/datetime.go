f00155b471559cfe43e2c0ca44dea7f7ae437e23
Marian Steinbach <marian@giantswarm.io>
2017-06-08 10:22:39 +0200

    Support parsing date format with offset (#73)
    * Adding a test case
    * Add UTC suffix to short date format
    * Adding parsing for alternative format

--- util/datetime.go
+++ util/datetime.go
@@ -6,7 +6,7 @@ import (
 )
 
 const (
-	shortFormat string = "2006 Jan 02, 15:04"
+	shortFormat string = "2006 Jan 02, 15:04 UTC"
 )
 
 // ParseDate parses our common date/time strings
@@ -14,12 +14,24 @@ const (
 func ParseDate(dateString string) time.Time {
 	// our standard format
 	template := "2006-01-02T15:04:05.000Z"
+	alternativeTemplate := "2006-01-02T15:04:05.000-07:00"
 
 	// normalizing the number of decimal places to 3
-	re := regexp.MustCompile("\\.[0-9]+Z$")
-	dateString = re.ReplaceAllLiteralString(dateString, ".000Z")
+	// and discarding sub-second detail along the way
+	re := regexp.MustCompile("\\.[0-9]+")
+	dateString = re.ReplaceAllLiteralString(dateString, ".000")
 
-	t, _ := time.Parse(template, dateString)
+	// try parsing with several formats
+	t, err := time.Parse(template, dateString)
+	if err == nil {
+		return t
+	}
+
+	t, err = time.Parse(alternativeTemplate, dateString)
+	if err == nil {
+		l, _ := time.LoadLocation("UTC")
+		t = t.In(l)
+	}
 	return t
 }
 
