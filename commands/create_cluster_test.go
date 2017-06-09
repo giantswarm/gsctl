e35cf00b4e6bfa3a728554852a31440e254e6f51
Marian Steinbach <marian@giantswarm.io>
2017-06-09 09:43:23 +0200

    Adding tests for cluster creation (#75)
    * Big refactoring of create_cluster.go: Separated business logic (validation, execution) and success/error output to make business logic easier testable
    * Adding more meaningful tests for cluster creation

--- commands/create_cluster_test.go
+++ commands/create_cluster_test.go
@@ -1,6 +1,27 @@
 package commands
 
-import "testing"
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+// TestReadFiles tests the readDefinitionFromFile with all
+// YAML files in the testdata directory
+func TestReadFiles(t *testing.T) {
+	basePath := "testdata"
+	files, _ := ioutil.ReadDir(basePath)
+	for _, f := range files {
+		path := basePath + "/" + f.Name()
+		_, err := readDefinitionFromFile(path)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+}
 
 func Test_CreateFromYAML01(t *testing.T) {
 	definition := clusterDefinition{}
@@ -40,7 +61,7 @@ workers:
   - cpu:
       cores: 2
     memory:
-      size_gb: 5
+      size_gb: 5.5
     storage:
       size_gb: 13
     labels:
@@ -56,8 +77,8 @@ workers:
 	if myDef.Workers[1].CPU.Cores != 2 {
 		t.Error("Expected myDef.Workers[1].CPU.Cores to be 2, got: ", myDef.Workers[1].CPU.Cores)
 	}
-	if myDef.Workers[1].Memory.SizeGB != 5.0 {
-		t.Error("Expected myDef.Workers[1].Memory.SizeGB to be 5, got: ", myDef.Workers[1].Memory.SizeGB)
+	if myDef.Workers[1].Memory.SizeGB != 5.5 {
+		t.Error("Expected myDef.Workers[1].Memory.SizeGB to be 5.5, got: ", myDef.Workers[1].Memory.SizeGB)
 	}
 	if myDef.Workers[1].Storage.SizeGB != 13.0 {
 		t.Error("Expected myDef.Workers[1].Storage.SizeGB to be 13, got: ", myDef.Workers[1].Storage.SizeGB)
@@ -74,5 +95,115 @@ func Test_CreateFromBadYAML01(t *testing.T) {
 	}
 	if myDef.Owner != "" {
 		t.Error("Expected owner to be empty, got: ", myDef.Owner)
+	}
+}
+
+// Test_CreateClusterSuccessfully tests cluster creations that should succeed
+func Test_CreateClusterSuccessfully(t *testing.T) {
+	// mock server always responding positively
+	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		t.Log("mockServer request: ", r.Method, r.URL)
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Location", "/v4/clusters/f6e8r/")
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(`{"code": "RESOURCE_CREATED", "message": "Yeah!"}`))
+	}))
+	defer mockServer.Close()
+
+	var testCases = []addClusterArguments{
+		// minimal arguments
+		addClusterArguments{
+			apiEndpoint: mockServer.URL,
+			owner:       "acme",
+			token:       "fake token",
+		},
+		// extensive arguments
+		addClusterArguments{
+			apiEndpoint:         mockServer.URL,
+			clusterName:         "UnitTestCluster",
+			numWorkers:          4,
+			kubernetesVersion:   "myK8sVersion",
+			owner:               "acme",
+			token:               "fake token",
+			workerNumCPUs:       3,
+			workerMemorySizeGB:  4,
+			workerStorageSizeGB: 10,
+			verbose:             true,
+		},
+		addClusterArguments{
+			apiEndpoint:       mockServer.URL,
+			clusterName:       "Cluster Name from Args",
+			owner:             "acme",
+			token:             "fake token",
+			inputYAMLFile:     "testdata/minimal.yaml",
+			kubernetesVersion: "K8sVersionFromArgs",
+			verbose:           true,
+		},
+	}
+
+	validateErr := errors.New("")
+	executeErr := errors.New("")
+
+	for i, testCase := range testCases {
+		validateErr = validatePreConditions(testCase)
+		if validateErr != nil {
+			t.Error(fmt.Sprintf("Validation error in testCase %v: %s", i, validateErr.Error()))
+		}
+		_, executeErr = addCluster(testCase)
+		if executeErr != nil {
+			t.Error(fmt.Sprintf("Execution error in testCase %v: %s", i, executeErr.Error()))
+		}
+	}
+}
+
+// Test_CreateClusterFailures tests for errors in cluster creations
+// (these attempts should never succeed)
+func Test_CreateClusterFailures(t *testing.T) {
+	// mock server always responding negatively
+	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		t.Log("mockServer request: ", r.Method, r.URL)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"code": "BAD_REQUEST", "message": "Something was fishy"}`))
+	}))
+	defer mockServer.Close()
+
+	var testCases = []addClusterArguments{
+		// not authenticated
+		addClusterArguments{
+			apiEndpoint: mockServer.URL,
+			owner:       "owner",
+			token:       "",
+		},
+		// extensive arguments (only a server error should let this fail)
+		addClusterArguments{
+			apiEndpoint:         mockServer.URL,
+			clusterName:         "UnitTestCluster",
+			numWorkers:          4,
+			kubernetesVersion:   "myK8sVersion",
+			owner:               "acme",
+			token:               "fake token",
+			workerNumCPUs:       3,
+			workerMemorySizeGB:  4,
+			workerStorageSizeGB: 10,
+			verbose:             true,
+		},
+		// file not readable
+		addClusterArguments{
+			apiEndpoint:   mockServer.URL,
+			token:         "fake token",
+			inputYAMLFile: "does/not/exist.yaml",
+			dryRun:        true,
+		},
+	}
+
+	for i, testCase := range testCases {
+		validateErr := validatePreConditions(testCase)
+		if validateErr == nil {
+			_, execErr := addCluster(testCase)
+			if execErr == nil {
+				t.Error(fmt.Sprintf("Expected errors didn't occur in testCase %v", i))
+			}
+		}
 	}
 }
