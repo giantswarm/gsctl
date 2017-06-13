013ae9af5f70ddb55f39a5429583f7439f263d27
Marian Steinbach <marian@giantswarm.io>
2017-06-13 22:34:54 +0200

    Adding test (for Pawel) ;-)

--- commands/info_test.go
+++ commands/info_test.go
@@ -2,23 +2,49 @@ package commands
 
 import (
 	"io/ioutil"
+	"os"
+	"strings"
 	"testing"
+
+	"github.com/spf13/viper"
 
 	"github.com/giantswarm/gsctl/config"
 )
 
 func Test_PrintInfo(t *testing.T) {
+	defer viper.Reset()
 	printInfo(InfoCommand, []string{})
 }
 
 func Test_PrintInfoVerbose(t *testing.T) {
+	defer viper.Reset()
 	cmdVerbose = true
 	printInfo(InfoCommand, []string{})
 }
 
-func Test_PrintInfoWithTempDirAndToken(t *testing.T) {
+// Test_InfoWithTempDirAndToken tests the info() function with a custom
+// configuration path and an auth-token
+func Test_InfoWithTempDirAndToken(t *testing.T) {
+	defer viper.Reset()
 	dir, _ := ioutil.TempDir("", config.ProgramName)
-	cmdConfigDirPath = dir
+	defer os.RemoveAll(dir)
+
+	// Normally cobra does this for us, but here we don't use cobra.
+	config.Initialize(dir)
+
 	cmdToken = "fake token"
-	printInfo(InfoCommand, []string{})
+	args := defaultInfoArguments()
+
+	infoResult := info(args)
+
+	if !strings.Contains(infoResult.configFilePath, dir) {
+		t.Errorf("Config file path not as expected: Got %s, expected %s",
+			infoResult.configFilePath, dir)
+	}
+	if infoResult.token != cmdToken {
+		t.Errorf("Expected token '%s', got '%s'", cmdToken, infoResult.token)
+	}
+	if infoResult.email != "" {
+		t.Error("Expected empty email, got ", infoResult.email)
+	}
 }
