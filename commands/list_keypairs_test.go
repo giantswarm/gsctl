123bd206b705467c865f38ac773d239482130eb5
Marian Steinbach <marian@giantswarm.io>
2017-06-14 16:32:50 +0200

    Testcases rewritten according to refactoring

--- commands/list_keypairs_test.go
+++ commands/list_keypairs_test.go
@@ -10,83 +10,139 @@ import (
 	"github.com/spf13/viper"
 )
 
-// TestNotLoggedIn checks whether we detect that the user is not logged in
-func Test_CheckListKeypairs_NotLoggedIn(t *testing.T) {
+// Test_ListKeypairs_NotLoggedIn checks whether we are detecting whether or not the user
+// is logged in or provides an auth token.
+func Test_ListKeypairs_NotLoggedIn(t *testing.T) {
 	defer viper.Reset()
 	dir := tempDir()
 	defer os.RemoveAll(dir)
 	config.Initialize(dir)
 	config.Config.Token = ""
 
-	err := checkListKeypairs(ListKeypairsCommand, []string{})
-	if err == nil || err.Error() != "You are not logged in. Use 'gsctl login' to log in." {
-		t.Error("Unexpected error:", err)
+	args := listKeypairsArguments{}
+	err := listKeypairsValidate(&args)
+	if err == nil {
+		t.Error("No error thrown where we expected an error.")
 	}
 }
 
-// TestLoggedIn checks whether we assume that the user is logged in
-func Test_CheckListKeypairs_LoggedIn(t *testing.T) {
+// Test_ListKeypairs_Empty simulates the situation where there are no
+// key pairs for a given cluster.
+func Test_ListKeypairs_Empty(t *testing.T) {
 	defer viper.Reset()
 	dir := tempDir()
 	defer os.RemoveAll(dir)
 	config.Initialize(dir)
-	config.Config.Token = "some-test-token"
+
+	// mock service returning empty key pair array.
+	keyPairsMockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`[]`))
+	}))
+	defer keyPairsMockServer.Close()
 
 	// needed to prevent search for the default cluster
-	cmdClusterID = "foobar"
-	err := checkListKeypairs(ListKeypairsCommand, []string{})
+	args := listKeypairsArguments{}
+	args.apiEndpoint = keyPairsMockServer.URL
+	args.token = "my-token"
+	args.clusterID = "my-cluster"
+
+	err := listKeypairsValidate(&args)
 	if err != nil {
-		t.Error("Login token not accepted:", err)
+		t.Error(err)
+	}
+
+	result, listErr := listKeypairs(args)
+	if listErr != nil {
+		t.Error(listErr)
+	}
+	if len(result.keypairs) > 0 {
+		t.Error("Got key pairs where we expected none.")
 	}
 }
 
-// Not logged in, but a cluster ID is given. An error should occur.
-func Test_CheckListKeypairs_NotLoggedInWithCluster(t *testing.T) {
-	defer viper.Reset()
-	dir := tempDir()
-	defer os.RemoveAll(dir)
-	config.Initialize(dir)
-	config.Config.Token = ""
-
-	cmdClusterID = "foobar"
-	err := checkListKeypairs(ListKeypairsCommand, []string{})
-	if err == nil {
-		t.Error("Expected error didn't occur, err was nil.")
-	}
-}
-
-func Test_ListKeyPairs(t *testing.T) {
+// Test_ListKeypairs_NotFound simulates the situation where the cluster
+// to list key pairs for is not found
+func Test_ListKeypairs_NotFound(t *testing.T) {
 	defer viper.Reset()
 	dir := tempDir()
 	defer os.RemoveAll(dir)
 	config.Initialize(dir)
 
-	// mock service returning key pairs. For the sake of cimplicity,
+	keyPairsMockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(`{"code": "RESOURCE_NOT_FOUND", "message": "The cluster could not be found."}`))
+	}))
+	defer keyPairsMockServer.Close()
+
+	args := listKeypairsArguments{}
+	args.apiEndpoint = keyPairsMockServer.URL
+	args.token = "my-token"
+	args.clusterID = "unknown-cluster"
+
+	err := listKeypairsValidate(&args)
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, listErr := listKeypairs(args)
+	if listErr == nil {
+		t.Error("No error occurred where we expected one.")
+	} else if listErr.Error() != errClusterNotFound {
+		t.Errorf("Expected error '%s', got '%s'.", errClusterNotFound, listErr.Error())
+	}
+}
+
+// Test_ListKeyPairs_Nonempty simulates listing key pairs where several
+// items are returned.
+func Test_ListKeyPairs_Nonempty(t *testing.T) {
+	defer viper.Reset()
+	dir := tempDir()
+	defer os.RemoveAll(dir)
+	config.Initialize(dir)
+
+	// mock service returning key pairs. For the sake of simplicity,
 	// it doesn't care about auth tokens.
 	keyPairsMockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(`[
+			{
+        "create_date": "2017-01-23T13:57:57.755631763Z",
+        "description": "Added by user oliver.ponder@gmail.com using Happa web interface",
+        "id": "74:2d:de:d2:6b:9f:4d:a5:e5:0d:eb:6e:98:14:02:6c:79:40:f6:58",
+        "ttl_hours": 720
+      },
       {
         "create_date": "2017-03-17T12:41:23.053271166Z",
         "description": "Added by user marian@sendung.de using 'gsctl create kubeconfig'",
         "id": "52:64:7d:ca:75:3c:7b:46:06:2f:a0:ce:42:9a:76:c9:2b:76:aa:9e",
-        "ttl_hours": 720
-      },
-      {
-        "create_date": "2017-01-23T13:57:57.755631763Z",
-        "description": "Added by user oliver.ponder@gmail.com using Happa web interface",
-        "id": "74:2d:de:d2:6b:9f:4d:a5:e5:0d:eb:6e:98:14:02:6c:79:40:f6:58",
         "ttl_hours": 720
       }
     ]`))
 	}))
 	defer keyPairsMockServer.Close()
 
-	cmdAPIEndpoint = keyPairsMockServer.URL
-	cmdClusterID = "test-cluster-id"
-	_, err := keypairsTable()
+	args := listKeypairsArguments{}
+	args.apiEndpoint = keyPairsMockServer.URL
+	args.token = "my-token"
+	args.clusterID = "my-cluster"
+
+	err := listKeypairsValidate(&args)
 	if err != nil {
 		t.Error(err)
+	}
+
+	result, listErr := listKeypairs(args)
+	if listErr != nil {
+		t.Error(listErr)
+	}
+	if len(result.keypairs) != 2 {
+		t.Errorf("We expected %d key pairs, got %d", 2, len(result.keypairs))
+	}
+	if result.keypairs[1].Id != "52:64:7d:ca:75:3c:7b:46:06:2f:a0:ce:42:9a:76:c9:2b:76:aa:9e" {
+		t.Error("Keypairs returned were not in the expected order.")
 	}
 }
