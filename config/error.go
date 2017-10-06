efe12434225ff0cbc93d64382e6506372d3e42fd
Marian Steinbach <marian@giantswarm.io>
2017-10-06 11:37:30 +0200

    Added specific errors happening in config

---
+++ config/error.go
@@ -0,0 +1,19 @@
+package config
+
+import "github.com/juju/errgo"
+
+// the user tries to use an endpoint that is not defined
+var endpointNotDefinedError = errgo.New("the given endpoint is not defined")
+
+// IsEndpointNotDefinedError asserts endpointNotDefinedError.
+func IsEndpointNotDefinedError(err error) bool {
+	return errgo.Cause(err) == endpointNotDefinedError
+}
+
+// attempt to store incomplete credentials in the config
+var credentialsRequiredError = errgo.New("email and password must not be empty")
+
+// IsCredentialsRequiredError asserts credentialsRequiredError.
+func IsCredentialsRequiredError(err error) bool {
+	return errgo.Cause(err) == credentialsRequiredError
+}
