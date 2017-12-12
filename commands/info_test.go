cb0f1a3ee5f62bc6ce9be1ad487e23145e532e75
Marian Steinbach <marian@giantswarm.io>
2017-12-12 10:57:44 +0100

    Add test config to tests for "gsctl info"

--- commands/info_test.go
+++ commands/info_test.go
@@ -9,12 +9,52 @@ import (
 // Test_PrintInfo simply executes the printInfo function.
 // TODO: actually test what this does
 func Test_PrintInfo(t *testing.T) {
+	// our test config YAML
+	yamlText := `last_version_check: 0001-01-01T00:00:00Z
+updated: 2017-09-29T11:23:15+02:00
+endpoints:
+  https://myapi.domain.tld:
+    email: email@example.com
+    token: some-token
+    alias: myalias
+  https://other.endpoint:
+    email: ""
+    token: ""
+    alias: ""
+selected_endpoint: https://other.endpoint`
+
+	dir, err := tempConfig(yamlText)
+	if err != nil {
+		t.Error(err)
+	}
+	defer os.RemoveAll(dir)
+
 	printInfo(InfoCommand, []string{})
 }
 
 // Test_PrintInfoVerbose simply executes the printInfo function with verbose=true
 // TODO: actually test what this does
 func Test_PrintInfoVerbose(t *testing.T) {
+	// our test config YAML
+	yamlText := `last_version_check: 0001-01-01T00:00:00Z
+updated: 2017-09-29T11:23:15+02:00
+endpoints:
+  https://myapi.domain.tld:
+    email: email@example.com
+    token: some-token
+    alias: myalias
+  https://other.endpoint:
+    email: ""
+    token: ""
+    alias: ""
+selected_endpoint: https://other.endpoint`
+
+	dir, err := tempConfig(yamlText)
+	if err != nil {
+		t.Error(err)
+	}
+	defer os.RemoveAll(dir)
+
 	cmdVerbose = true
 	printInfo(InfoCommand, []string{})
 }
