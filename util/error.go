60eddf650aa2cc337742b818037c677ab98d0d2b
Marian Steinbach <marian@giantswarm.io>
2018-07-17 15:47:55 +0200

    Fix complaints in the util package

--- util/error.go
+++ util/error.go
@@ -2,6 +2,7 @@ package util
 
 import "github.com/juju/errgo"
 
+// CouldNotSetKubectlClusterError is used if kubectl config set-cluster could not be executed.
 var CouldNotSetKubectlClusterError = errgo.New("could not set cluster using 'kubectl config set-cluster'")
 
 // IsCouldNotSetKubectlClusterError asserts CouldNotSetKubectlClusterError.
@@ -9,6 +10,7 @@ func IsCouldNotSetKubectlClusterError(err error) bool {
 	return errgo.Cause(err) == CouldNotSetKubectlClusterError
 }
 
+// CouldNotSetKubectlCredentialsError is used when kubectl config set-credentials could not be executed.
 var CouldNotSetKubectlCredentialsError = errgo.New("could not set credentials using 'kubectl config set-credentials'")
 
 // IsCouldNotSetKubectlCredentialsError asserts CouldNotSetKubectlClusterError.
@@ -16,6 +18,7 @@ func IsCouldNotSetKubectlCredentialsError(err error) bool {
 	return errgo.Cause(err) == CouldNotSetKubectlCredentialsError
 }
 
+// CouldNotSetKubectlContextError is used when kubectl config set-context could not be executed.
 var CouldNotSetKubectlContextError = errgo.New("could not set context using 'kubectl config set-context''")
 
 // IsCouldNotSetKubectlContextError asserts CouldNotSetKubectlContextError.
@@ -23,6 +26,7 @@ func IsCouldNotSetKubectlContextError(err error) bool {
 	return errgo.Cause(err) == CouldNotSetKubectlContextError
 }
 
+// CouldNotUseKubectlContextError is used when kubectl config use-context could not be executed.
 var CouldNotUseKubectlContextError = errgo.New("could not apply context using 'kubectl config use-context'")
 
 // IsCouldNotUseKubectlContextError asserts CouldNotUseKubectlContextError.
@@ -30,6 +34,7 @@ func IsCouldNotUseKubectlContextError(err error) bool {
 	return errgo.Cause(err) == CouldNotUseKubectlContextError
 }
 
+// InvalidDurationStringError is used when a duration string given by the user could not be parsed.
 var InvalidDurationStringError = errgo.New("could not parse duration string")
 
 // IsInvalidDurationStringError asserts InvalidDurationStringError.
