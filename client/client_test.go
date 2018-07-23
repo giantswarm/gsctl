b7a947a93f70058c7d9257d3bcc9dce345f8ee24
Marian Steinbach <marian@giantswarm.io>
2018-07-23 10:10:31 +0200

    Remove legacy tests relying on old GetUserOrganizations function

--- client/client_test.go
+++ client/client_test.go
@@ -12,69 +12,6 @@ import (
 	"github.com/giantswarm/gsctl/client/clienterror"
 )
 
-// TODO: remove once we have gotten rid of the legacy client
-func TestTimeout(t *testing.T) {
-	// Our test server.
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// enforce a timeout longer than the client's
-		time.Sleep(3 * time.Second)
-		fmt.Fprintln(w, "Hello")
-	}))
-	defer ts.Close()
-
-	clientConfig := Configuration{
-		Endpoint: ts.URL,
-		Timeout:  1 * time.Second,
-	}
-	apiClient, clientErr := New(clientConfig)
-	if clientErr != nil {
-		t.Error(clientErr)
-	}
-	_, _, err := apiClient.GetUserOrganizations("foo")
-	if err == nil {
-		t.Error("Expected Timeout error, got nil")
-	} else {
-		if err, ok := err.(net.Error); ok && !err.Timeout() {
-			t.Error("Expected Timeout error, got", err)
-		}
-	}
-}
-
-// TestUserAgent tests whether request have the proper User-Agent header
-// and if ParseGenericResponse works as expected.
-// TODO: remove once we have gotten rid of the legacy client
-func TestUserAgent(t *testing.T) {
-	// Our test server.
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Return valid JSON containing user agent string received
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(`{"code": "BAD_REQUEST", "message": "user-agent: ` + r.Header.Get("User-Agent") + `"}`))
-	}))
-	defer ts.Close()
-
-	clientConfig := Configuration{
-		Endpoint:  ts.URL,
-		UserAgent: "my own user agent/1.0",
-	}
-	apiClient, clientErr := New(clientConfig)
-	if clientErr != nil {
-		t.Error(clientErr)
-	}
-	// We use GetUserOrganizations just to issue a request. We could use any other
-	// API call, it wouldn't matter.
-	_, apiResponse, _ := apiClient.GetUserOrganizations("foo")
-
-	gr, err := ParseGenericResponse(apiResponse.Payload)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if !strings.Contains(gr.Message, clientConfig.UserAgent) {
-		t.Error("UserAgent string could not be found")
-	}
-}
-
 // TestRedactPasswordArgs tests redactPasswordArgs().
 func TestRedactPasswordArgs(t *testing.T) {
 	argtests := []struct {
