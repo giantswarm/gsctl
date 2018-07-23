98ae39a5238bda35b8246baa10c2a09f9b3d5c82
Marian Steinbach <marian@giantswarm.io>
2018-07-23 10:06:19 +0200

    Remove legacy wrapper func

--- client/legacy.go
+++ client/legacy.go
@@ -89,12 +89,6 @@ func (c *Wrapper) GetClusters(activityName string) ([]gsclientgenv1.V4ClusterLis
 	return response, apiResponse, err
 }
 
-// GetUserOrganizations calls the getUserOrganization operation via the old client.
-func (c *Wrapper) GetUserOrganizations(activityName string) ([]gsclientgenv1.V4OrganizationListItem, *gsclientgenv1.APIResponse, error) {
-	response, apiResponse, err := c.client.GetUserOrganizations(c.authHeader, c.requestID, activityName, getCommandLine())
-	return response, apiResponse, err
-}
-
 // ModifyCluster calls the modifyCluster operation via the old client.
 func (c *Wrapper) ModifyCluster(clusterID string, body gsclientgenv1.V4ModifyClusterRequest, activityName string) (*gsclientgenv1.V4ClusterDetailsModel, *gsclientgenv1.APIResponse, error) {
 	response, apiResponse, err := c.client.ModifyCluster(c.authHeader, clusterID, body, c.requestID, activityName, getCommandLine())
