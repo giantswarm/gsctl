4168545d78c473905e320fb0a6d0c5fefa4695e9
Marian Steinbach <marian@giantswarm.io>
2018-08-01 15:04:15 +0200

    Add verbose output to login_sso.go

--- commands/login_sso.go
+++ commands/login_sso.go
@@ -3,8 +3,11 @@ package commands
 import (
 	"fmt"
 	"math/rand"
+	"net/http"
 	"time"
 
+	"github.com/fatih/color"
+	"github.com/giantswarm/gsctl/client/clienterror"
 	"github.com/giantswarm/gsctl/config"
 	"github.com/giantswarm/gsctl/pkce"
 	"github.com/giantswarm/microerror"
@@ -19,8 +22,9 @@ func loginSSO(args loginArguments) (loginResult, error) {
 
 	pkceResponse, err := pkce.Run()
 	if err != nil {
-		fmt.Printf("DEBUG Error in pkce.Run(): %#v\n", err)
-		fmt.Printf("DEBUG pkce.Run() pkceResponse.ErrorDescription: %#v\n", pkceResponse.ErrorDescription)
+		if args.verbose {
+			fmt.Println(color.WhiteString("Attempt to run the OAuth2 PKCE workflow with a local callback HTTP server failed."))
+		}
 		return loginResult{}, microerror.Maskf(ssoError, pkceResponse.ErrorDescription)
 	}
 
@@ -33,17 +37,35 @@ func loginSSO(args loginArguments) (loginResult, error) {
 	// Check if the access token works by fetching the installation's name.
 	alias, err := getAlias(args.apiEndpoint, "Bearer", pkceResponse.AccessToken)
 	if err != nil {
-		fmt.Printf("DEBUG Error in getAlias: %#v\n", err)
-		return loginResult{}, microerror.Maskf(ssoError, "Unable to fetch installation information. Our api might be experiencing difficulties")
+		if args.verbose {
+			fmt.Println(color.WhiteString("Attempt to use new token against the API failed."))
+			if cErr, ok := err.(*clienterror.APIError); ok {
+				fmt.Println(color.WhiteString("Underlying error details: %s", cErr.OriginalError.Error()))
+			} else {
+				fmt.Println(color.WhiteString("Error details: %s", err.Error()))
+			}
+		}
+
+		if clientErr, ok := err.(*clienterror.APIError); ok {
+			if clientErr.HTTPStatusCode == http.StatusForbidden {
+				return loginResult{}, microerror.Mask(accessForbiddenError)
+			}
+
+			return loginResult{}, clientErr
+		}
+
+		return loginResult{}, microerror.Maskf(ssoError, err.Error())
 	}
 
 	// Store the token in the config file.
 	if err := config.Config.StoreEndpointAuth(args.apiEndpoint, alias, idToken.Email, "Bearer", pkceResponse.AccessToken); err != nil {
-		fmt.Printf("DEBUG Error in StoreEndpointAuth: %#v\n", err)
+		if args.verbose {
+			fmt.Println(color.WhiteString("Attempt to store our authentication data with the endpoint in the configuration failed."))
+			fmt.Println(color.WhiteString("Error details: %s", err.Error()))
+		}
 		return loginResult{}, microerror.Maskf(ssoError, "Error while attempting to store the token in the config file")
 	}
 	if err := config.Config.SelectEndpoint(args.apiEndpoint); err != nil {
-		fmt.Printf("DEBUG Error in SelectEndpoint: %#v\n", err)
 		return loginResult{}, microerror.Mask(err)
 	}
 
