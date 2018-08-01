cacba9a73b88fe49b24e831c94e8551527376c54
Tim Schindler <xh3b4sd@gmail.com>
2018-08-01 15:22:20 +0200

    rewrites errors

--- config/error.go
+++ config/error.go
@@ -1,42 +1,50 @@
 package config
 
-import "github.com/juju/errgo"
+import "github.com/giantswarm/microerror"
 
 // endpointNotDefinedError means the user tries to use an endpoint that is not defined
-var endpointNotDefinedError = errgo.New("the given endpoint is not defined")
+var endpointNotDefinedError = &microerror.Error{
+	Kind: "endpointNotDefinedError",
+}
 
 // IsEndpointNotDefinedError asserts endpointNotDefinedError.
 func IsEndpointNotDefinedError(err error) bool {
-	return errgo.Cause(err) == endpointNotDefinedError
+	return microerror.Cause(err) == endpointNotDefinedError
 }
 
 // aliasMustBeUniqueError should be used if the user tries to add an alias to
 // an endpoint, but the alias is already in use
-var aliasMustBeUniqueError = errgo.New("alias must be unique")
+var aliasMustBeUniqueError = &microerror.Error{
+	Kind: "aliasMustBeUniqueError",
+}
 
 // IsAliasMustBeUniqueError asserts aliasMustBeUniqueError.
 func IsAliasMustBeUniqueError(err error) bool {
-	return errgo.Cause(err) == aliasMustBeUniqueError
+	return microerror.Cause(err) == aliasMustBeUniqueError
 }
 
 // credentialsRequiredError means an attempt to store incomplete credentials in the config
-var credentialsRequiredError = errgo.New("email, password, or token must not be empty")
+var credentialsRequiredError = microerror.New("email, password, or token must not be empty")
 
 // IsCredentialsRequiredError asserts credentialsRequiredError.
 func IsCredentialsRequiredError(err error) bool {
-	return errgo.Cause(err) == credentialsRequiredError
+	return microerror.Cause(err) == credentialsRequiredError
 }
 
-var garbageCollectionFailedError = errgo.New("garbage collection failed")
+var garbageCollectionFailedError = &microerror.Error{
+	Kind: "garbageCollectionFailedError",
+}
 
 // IsGarbageCollectionFailedError asserts garbageCollectionFailedError.
 func IsGarbageCollectionFailedError(err error) bool {
-	return errgo.Cause(err) == garbageCollectionFailedError
+	return microerror.Cause(err) == garbageCollectionFailedError
 }
 
-var garbageCollectionPartiallyFailedError = errgo.New("garbage collection partially failed")
+var garbageCollectionPartiallyFailedError = &microerror.Error{
+	Kind: "garbageCollectionPartiallyFailedError",
+}
 
 // IsGarbageCollectionPartiallyFailedError asserts garbageCollectionPartiallyFailedError.
 func IsGarbageCollectionPartiallyFailedError(err error) bool {
-	return errgo.Cause(err) == garbageCollectionPartiallyFailedError
+	return microerror.Cause(err) == garbageCollectionPartiallyFailedError
 }
