05c45188c9409dddfb8734a549d1e3b41af87306
Marian Steinbach <marian@giantswarm.io>
2019-05-13 12:12:29 +0200

    Dependency upgrade

--- vendor/github.com/giantswarm/gsclientgen/client/users/get_current_user_responses.go
+++ vendor/github.com/giantswarm/gsclientgen/client/users/get_current_user_responses.go
@@ -61,7 +61,7 @@ func NewGetCurrentUserOK() *GetCurrentUserOK {
 Success
 */
 type GetCurrentUserOK struct {
-	Payload *models.GetUserOKBody
+	Payload *models.V4UserListItem
 }
 
 func (o *GetCurrentUserOK) Error() string {
@@ -70,7 +70,7 @@ func (o *GetCurrentUserOK) Error() string {
 
 func (o *GetCurrentUserOK) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
 
-	o.Payload = new(models.GetUserOKBody)
+	o.Payload = new(models.V4UserListItem)
 
 	// response payload
 	if err := consumer.Consume(response.Body(), o.Payload); err != nil && err != io.EOF {
