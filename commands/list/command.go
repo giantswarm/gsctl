2b891392f42e2972f9211da95b6a36582170754a
Marian Steinbach <marian@giantswarm.io>
2019-07-15 17:53:05 +0200

    Add 'list nodepools' command (#373)

--- commands/list/command.go
+++ commands/list/command.go
@@ -7,6 +7,7 @@ import (
 	"github.com/giantswarm/gsctl/commands/list/clusters"
 	"github.com/giantswarm/gsctl/commands/list/endpoints"
 	"github.com/giantswarm/gsctl/commands/list/keypairs"
+	"github.com/giantswarm/gsctl/commands/list/nodepools"
 	"github.com/giantswarm/gsctl/commands/list/organizations"
 	"github.com/giantswarm/gsctl/commands/list/releases"
 )
@@ -15,8 +16,8 @@ var (
 	// Command is the command to list things.
 	Command = &cobra.Command{
 		Use:   "list",
-		Short: "List things, like organizations, clusters, key pairs",
-		Long:  `Prints a list of the things you have access to`,
+		Short: "List things, like organizations, clusters, node pools, key pairs",
+		Long:  `Prints a list of the things you have access to.`,
 	}
 )
 
@@ -24,6 +25,7 @@ func init() {
 	Command.AddCommand(clusters.Command)
 	Command.AddCommand(endpoints.Command)
 	Command.AddCommand(keypairs.Command)
+	Command.AddCommand(nodepools.Command)
 	Command.AddCommand(organizations.Command)
 	Command.AddCommand(releases.Command)
 }
---
+++ commands/list/nodepools/command.go
@@ -0,0 +1,224 @@
+// Package nodepools implements the 'list nodepools' sub-command.
+package nodepools
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/fatih/color"
+	"github.com/giantswarm/columnize"
+	"github.com/giantswarm/gsclientgen/models"
+	"github.com/giantswarm/microerror"
+	"github.com/spf13/cobra"
+
+	"github.com/giantswarm/gsctl/client"
+	"github.com/giantswarm/gsctl/client/clienterror"
+	"github.com/giantswarm/gsctl/commands/errors"
+	"github.com/giantswarm/gsctl/config"
+	"github.com/giantswarm/gsctl/flags"
+	"github.com/giantswarm/gsctl/nodespec"
+)
+
+var (
+	// Command performs the "list nodepools" function
+	Command = &cobra.Command{
+		Use:     "nodepools <cluster-id>",
+		Aliases: []string{"nps", "np"},
+
+		// Args: cobra.ExactArgs(1) guarantees that cobra will fail if no positional argument is given.
+		Args:  cobra.ExactArgs(1),
+		Short: "List node pools",
+		Long: `Prints a list of the node pools of a cluster.
+
+The result will be a table of all node pools of a specific cluster with the following details in
+columns:
+
+	ID:             Node pool identifier (unique within the cluster)
+	NAME:           Name specified for the node pool, usually indicating the purpose
+	AZ:             Availability zone letters used by the node pool, separated by comma
+	INSTANCE TYPE:  EC2 instance type used for worker nodes
+	NODES MIN/MAX:  The minimum and maximum number of worker nodes in this pool
+	NODES DESIRED:  Current desired number of nodes as determined by the autoscaler
+	NODES READY:    Number of nodes that are in the Ready state in kubernetes
+	CPUS:           Sum of CPU cores in nodes that are in state Ready
+	RAM (GB):       Sum of memory in GB of all nodes that are in state Ready
+
+To see all available details for a cluster, use 'gsctl show nodepool <cluster-id>/<nodepool-id>'.
+
+To list all clusters you have access to, use 'gsctl list clusters'.`,
+		PreRun: printValidation,
+		Run:    printResult,
+	}
+)
+
+const activityName = "list-nodepools"
+
+type arguments struct {
+	apiEndpoint string
+	authToken   string
+	scheme      string
+	clusterID   string
+}
+
+// resultRow represents one nope pool row as returned by fetchNodePools.
+type resultRow struct {
+	// nodePool contains all the node pool details as returned from the API.
+	nodePool *models.V5GetNodePoolsResponseItems
+	// instanceTypeDetails contains details on the instance type.
+	instanceTypeDetails *nodespec.InstanceType
+	sumCPUs             int64
+	sumMemory           float64
+}
+
+// defaultArgs creates arguments based on command line flags and config.
+func defaultArgs(cmdLineArgs []string) arguments {
+	endpoint := config.Config.ChooseEndpoint(flags.CmdAPIEndpoint)
+	token := config.Config.ChooseToken(endpoint, flags.CmdToken)
+	scheme := config.Config.ChooseScheme(endpoint, flags.CmdToken)
+
+	return arguments{
+		apiEndpoint: endpoint,
+		authToken:   token,
+		scheme:      scheme,
+		clusterID:   cmdLineArgs[0],
+	}
+}
+
+func verifyPreconditions(args arguments, positionalArgs []string) error {
+	if config.Config.Token == "" && args.authToken == "" {
+		return microerror.Mask(errors.NotLoggedInError)
+	}
+
+	return nil
+}
+
+func printValidation(cmd *cobra.Command, positionalArgs []string) {
+	args := defaultArgs(positionalArgs)
+	err := verifyPreconditions(args, positionalArgs)
+	if err == nil {
+		return
+	}
+
+	errors.HandleCommonErrors(err)
+}
+
+// fetchNodePools collects all information we would want to display
+// on the node pools of a cluster.
+func fetchNodePools(args arguments) ([]*resultRow, error) {
+	clientV2, err := client.NewWithConfig(flags.CmdAPIEndpoint, flags.CmdToken)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	auxParams := clientV2.DefaultAuxiliaryParams()
+	auxParams.ActivityName = activityName
+
+	response, err := clientV2.GetNodePools(args.clusterID, auxParams)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	// sort node pools by ID
+	sort.Slice(response.Payload[:], func(i, j int) bool {
+		return response.Payload[i].ID < response.Payload[j].ID
+	})
+
+	awsInfo, err := nodespec.NewAWS()
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	// create combined output data structure.
+	rows := []*resultRow{}
+
+	for _, np := range response.Payload {
+		it, err := awsInfo.GetInstanceTypeDetails(np.NodeSpec.Aws.InstanceType)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+
+		sumCPUs := np.Status.NodesReady * int64(it.CPUCores)
+		sumMemory := float64(np.Status.NodesReady) * float64(it.MemorySizeGB)
+
+		rows = append(rows, &resultRow{np, it, sumCPUs, sumMemory})
+	}
+
+	return rows, nil
+
+}
+
+func printResult(cmd *cobra.Command, positionalArgs []string) {
+	args := defaultArgs(positionalArgs)
+	nodePools, err := fetchNodePools(args)
+	if err != nil {
+		errors.HandleCommonErrors(err)
+		client.HandleErrors(err)
+
+		if clientErr, ok := err.(*clienterror.APIError); ok {
+			fmt.Println(color.RedString(clientErr.ErrorMessage))
+			if clientErr.ErrorDetails != "" {
+				fmt.Println(clientErr.ErrorDetails)
+			}
+		} else {
+			fmt.Println(color.RedString("Error: %s", err.Error()))
+		}
+		os.Exit(1)
+	}
+
+	table := []string{}
+
+	headers := []string{
+		color.CyanString("ID"),
+		color.CyanString("NAME"),
+		color.CyanString("AZ"),
+		color.CyanString("INSTANCE TYPE"),
+		color.CyanString("NODES MIN/MAX"),
+		color.CyanString("NODES DESIRED"),
+		color.CyanString("NODES READY"),
+		color.CyanString("CPUS"),
+		color.CyanString("RAM (GB)"),
+	}
+	table = append(table, strings.Join(headers, "|"))
+
+	for _, row := range nodePools {
+		table = append(table, strings.Join([]string{
+			row.nodePool.ID,
+			row.nodePool.Name,
+			formatAvailabilityZones(row.nodePool.AvailabilityZones),
+			row.nodePool.NodeSpec.Aws.InstanceType,
+			strconv.FormatInt(row.nodePool.Scaling.Min, 10) + "/" + strconv.FormatInt(row.nodePool.Scaling.Max, 10),
+			strconv.FormatInt(row.nodePool.Status.Nodes, 10),
+			formatNodesReady(row.nodePool.Status.Nodes, row.nodePool.Status.NodesReady),
+			strconv.FormatInt(row.sumCPUs, 10),
+			strconv.FormatFloat(row.sumMemory, 'f', 1, 64),
+		}, "|"))
+	}
+
+	fmt.Println(columnize.SimpleFormat(table))
+}
+
+// formatAvailabilityZones returns the list of availability zones
+// as one string consisting of uppercase letters only, e. g. "A,B,C".
+func formatAvailabilityZones(az []string) string {
+	shortened := []string{}
+
+	for _, az := range az {
+		// last character of each item
+		shortened = append(shortened, az[len(az)-1:])
+	}
+
+	sort.Strings(shortened)
+
+	return strings.ToUpper(strings.Join(shortened, ","))
+}
+
+func formatNodesReady(nodes, nodesReady int64) string {
+	if nodes == nodesReady {
+		return strconv.FormatInt(nodesReady, 10)
+	}
+
+	return color.YellowString(strconv.FormatInt(nodesReady, 10))
+}
