ebb57ec5e637047c3216a5bc2d790802397815b0
Marian Steinbach <marian@giantswarm.io>
2019-07-23 12:13:26 +0200

    Clean up table tests

--- formatting/availability_zones_test.go
+++ formatting/availability_zones_test.go
@@ -1,20 +1,25 @@
 package formatting
 
-import "testing"
-
-var flagtests = []struct {
-	in  []string
-	out string
-}{
-	{[]string{"foo1-c"}, "C"},
-	{[]string{"foo1-b", "foo1-a"}, "A,B"},
-}
+import (
+	"strconv"
+	"testing"
+)
 
 func TestAvailabilityZonesList(t *testing.T) {
-	for _, tt := range flagtests {
-		s := AvailabilityZonesList(tt.in)
-		if s != tt.out {
-			t.Errorf("got %q, want %q", s, tt.out)
-		}
+	var testCases = []struct {
+		in  []string
+		out string
+	}{
+		{[]string{"foo1-c"}, "C"},
+		{[]string{"foo1-b", "foo1-a"}, "A,B"},
+	}
+
+	for i, tt := range testCases {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			s := AvailabilityZonesList(tt.in)
+			if s != tt.out {
+				t.Errorf("got %q, want %q", s, tt.out)
+			}
+		})
 	}
 }
--- formatting/key_pair_id_test.go
+++ formatting/key_pair_id_test.go
@@ -1,21 +1,26 @@
 package formatting
 
-import "testing"
+import (
+	"strconv"
+	"testing"
+)
 
 type KeypairIDTest struct {
 	input  string
 	output string
 }
 
-var keypairIDTests = []KeypairIDTest{
-	{"a1:b2:c3:d4:e5:f6:g7:00", "a1b2c3d4e5f6g700"},
-}
-
 func TestCleanKeypairID(t *testing.T) {
-	for i, test := range keypairIDTests {
-		out := CleanKeypairID(test.input)
-		if out != test.output {
-			t.Errorf("#%d: CleanKeypairID(%s) = '%s'; want '%s'", i, test.input, out, test.output)
-		}
+	testCases := []KeypairIDTest{
+		{"a1:b2:c3:d4:e5:f6:g7:00", "a1b2c3d4e5f6g700"},
+	}
+
+	for i, test := range testCases {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			out := CleanKeypairID(test.input)
+			if out != test.output {
+				t.Errorf("#%d: CleanKeypairID(%s) = '%s'; want '%s'", i, test.input, out, test.output)
+			}
+		})
 	}
 }
