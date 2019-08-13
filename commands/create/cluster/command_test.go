f7c340eba453fea6f12487b095290062c3bd919e
Marian Steinbach <marian@giantswarm.io>
2019-08-13 16:25:36 +0200

    Add test for collectArguments

--- commands/create/cluster/command_test.go
+++ commands/create/cluster/command_test.go
@@ -3,21 +3,110 @@ package cluster
 import (
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"strings"
 	"testing"
 
 	"github.com/giantswarm/microerror"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/spf13/afero"
 	yaml "gopkg.in/yaml.v2"
 
 	"github.com/giantswarm/gsctl/commands/errors"
 	"github.com/giantswarm/gsctl/commands/types"
 	"github.com/giantswarm/gsctl/flags"
+	"github.com/giantswarm/gsctl/testutils"
 )
 
-// TestReadFiles tests the readDefinitionFromFile with all
+// configYAML is a mock configuration used by some of the tests.
+const configYAML = `last_version_check: 0001-01-01T00:00:00Z
+endpoints:
+  https://foo:
+    email: email@example.com
+    token: some-token
+selected_endpoint: https://foo
+updated: 2017-09-29T11:23:15+02:00
+`
+
+// Test_CollectArgs tests whether collectArguments produces the expected results.
+func Test_CollectArgs(t *testing.T) {
+	var testCases = []struct {
+		// The flags we pass to the command.
+		flags []string
+		// What we expect as arguments.
+		resultingArgs Arguments
+	}{
+		{
+			[]string{""},
+			Arguments{
+				APIEndpoint: "https://foo",
+				AuthToken:   "some-token",
+				Scheme:      "giantswarm",
+			},
+		},
+		{
+			[]string{
+				"--owner=acme",
+				"--availability-zones=2",
+				"--name=ClusterName",
+				"--release=1.2.3",
+				"--num-workers=5",
+				"--workers-min=5",
+				"--workers-max=10",
+				"--aws-instance-type=m10.impossible",
+				"--azure-vm-size=DoesNotExist",
+				"--num-cpus=4",
+				"--memory-gb=20",
+				"--storage-gb=40",
+				"--dry-run=true",
+			},
+			Arguments{
+				APIEndpoint:              "https://foo",
+				AuthToken:                "some-token",
+				AvailabilityZones:        2,
+				ClusterName:              "ClusterName",
+				DryRun:                   true,
+				NumWorkers:               5,
+				Owner:                    "acme",
+				ReleaseVersion:           "1.2.3",
+				Scheme:                   "giantswarm",
+				WorkerAwsEc2InstanceType: "m10.impossible",
+				WorkerAzureVMSize:        "DoesNotExist",
+				WorkerMemorySizeGB:       20,
+				WorkerNumCPUs:            4,
+				WorkersMax:               10,
+				WorkersMin:               5,
+				WorkerStorageSizeGB:      40,
+			},
+		},
+	}
+
+	fs := afero.NewMemMapFs()
+	_, err := testutils.TempConfig(fs, configYAML)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for i, tc := range testCases {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			initFlags()
+			Command.ParseFlags(tc.flags)
+
+			args := collectArguments()
+			if err != nil {
+				t.Errorf("Case %d - Unexpected error '%s'", i, err)
+			}
+			if diff := cmp.Diff(tc.resultingArgs, args, cmpopts.IgnoreUnexported(Arguments{})); diff != "" {
+				t.Errorf("Case %d - Resulting args unequal. (-expected +got):\n%s", i, diff)
+			}
+		})
+	}
+}
+
+// Test_ReadDefinitionFiles tests the readDefinitionFromFile with all
 // YAML files in the testdata directory.
-func TestReadFiles(t *testing.T) {
+func Test_ReadDefinitionFiles(t *testing.T) {
 	basePath := "testdata"
 	fs := afero.NewOsFs()
 	files, _ := afero.ReadDir(fs, basePath)
@@ -183,7 +272,7 @@ func Test_CreateClusterSuccessfully(t *testing.T) {
 			description: "Definition from YAML file",
 			inputArgs: &Arguments{
 				ClusterName:   "Cluster Name from Args",
-				FileSystem:    afero.NewOsFs(), // needed for YAML file access
+				fileSystem:    afero.NewOsFs(), // needed for YAML file access
 				InputYAMLFile: "testdata/minimal.yaml",
 				Owner:         "acme",
 				AuthToken:     "fake token",
@@ -280,7 +369,7 @@ func Test_CreateClusterExecutionFailures(t *testing.T) {
 			inputArgs: &Arguments{
 				Owner:         "owner",
 				AuthToken:     "some-token",
-				FileSystem:    afero.NewOsFs(),
+				fileSystem:    afero.NewOsFs(),
 				InputYAMLFile: "does/not/exist.yaml",
 				DryRun:        true,
 			},
