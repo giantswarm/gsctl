a27024ae6d21fa44e06aa31113d06e5471d41bd9
Marian Steinbach <marian@giantswarm.io>
2019-11-19 15:34:51 +0100

    Fix white space

--- commands/create/cluster/command.go
+++ commands/create/cluster/command.go
@@ -127,12 +127,12 @@ general defaults:
 - Release: the latest release is used.
 - Workers
   - On AWS and when using the latest release, and when not specifying node pool
-	details via a clsuter definition, the cluster will be created with a 
-	default node pool. You may define node pools in your cluster definition
-	YAML or add node pools one by one using 'gsctl create nodepool'.
+    details via a clsuter definition, the cluster will be created with a 
+    default node pool. You may define node pools in your cluster definition
+    YAML or add node pools one by one using 'gsctl create nodepool'.
   - On AWS with releases prior to node pools, and with Azure and KVM, the
     cluster will have three worker nodes by default, using pretty much the
-	minimal spec for a working cluster.
+    minimal spec for a working cluster.
   - Autoscaling will be inactive initially, as the minimum and maximum of the
     scaling range  will be set to 3.
   - All worker nodes will be in the same availability zone.
