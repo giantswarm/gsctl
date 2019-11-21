48e89ab07093b63e3ec2b86d87c758bf82424b62
Marian Steinbach <marian@giantswarm.io>
2019-11-21 16:15:38 +0100

    Clean up one test

--- commands/upgrade/cluster/command_test.go
+++ commands/upgrade/cluster/command_test.go
@@ -13,25 +13,25 @@ import (
 	"github.com/giantswarm/gsctl/testutils"
 )
 
-var successorVersionTests = []struct {
-	myVersion        string
-	allVersions      []string
-	successorVersion string
-}{
-	{
-		"1.2.3",
-		[]string{"1.2.3", "1.2.4", "1.2.5", "10.2.3", "2.0.0", "0.1.0"},
-		"1.2.4",
-	},
-	// none of the versions in the slice is higher
-	{
-		"4.5.2",
-		[]string{"3.2.1", "0.5.1", "0.5.0", "0.6.0", "4.5.2"},
-		"",
-	},
-}
+func Test_successorReleaseVersion(t *testing.T) {
+	var successorVersionTests = []struct {
+		myVersion        string
+		allVersions      []string
+		successorVersion string
+	}{
+		{
+			"1.2.3",
+			[]string{"1.2.3", "1.2.4", "1.2.5", "10.2.3", "2.0.0", "0.1.0"},
+			"1.2.4",
+		},
+		// none of the versions in the slice is higher
+		{
+			"4.5.2",
+			[]string{"3.2.1", "0.5.1", "0.5.0", "0.6.0", "4.5.2"},
+			"",
+		},
+	}
 
-func TestSuccessorReleaseVersion(t *testing.T) {
 	for i, tc := range successorVersionTests {
 		v := successorReleaseVersion(tc.myVersion, tc.allVersions)
 		if v != tc.successorVersion {
