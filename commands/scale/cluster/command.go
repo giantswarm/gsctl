11c2ebd40ad6c38a8fe0932ef4be2e206a58f879
Bricktop <marcel.mueller1@rwth-aachen.de>
2020-01-16 11:12:03 +0100

    Check for provider not supported when scaling (#495)

--- commands/scale/cluster/command.go
+++ commands/scale/cluster/command.go
@@ -171,7 +171,7 @@ func verifyPreconditions(args Arguments) error {
 		}
 
 		_, err = clientWrapper.GetClusterV5(args.ClusterID, auxParams)
-		if errors.IsClusterNotFoundError(err) {
+		if errors.IsClusterNotFoundError(err) || errors.IsProviderNotSupportedError(err) {
 			// The cluster is not a v5 cluster. So do nothing.
 		} else if err != nil {
 			return microerror.Mask(err)
