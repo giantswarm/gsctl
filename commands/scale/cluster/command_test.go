bc6ed0b8a5a9c3b809a701bb7e8ab85abcf520e6
axbarsan <mnlk123@yahoo.com>
2020-05-06 19:43:07 +0200

    Refactor scaling test to accept multiple parameters

--- commands/scale/cluster/command_test.go
+++ commands/scale/cluster/command_test.go
@@ -1,6 +1,7 @@
 package cluster
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -11,10 +12,12 @@ import (
 
 	"github.com/Jeffail/gabs"
 	"github.com/giantswarm/gscliauth/config"
-	"github.com/giantswarm/gsctl/client"
+	"github.com/giantswarm/gsclientgen/models"
 	"github.com/giantswarm/microerror"
 	"github.com/google/go-cmp/cmp"
 	"github.com/spf13/afero"
+
+	"github.com/giantswarm/gsctl/client"
 
 	"github.com/giantswarm/gsctl/commands/errors"
 	"github.com/giantswarm/gsctl/testutils"
@@ -284,60 +287,57 @@ func TestScaleClusterNotLoggedIn(t *testing.T) {
 // TestScaleCluster tests scaling a cluster under normal conditions:
 // user logged in.
 func TestScaleCluster(t *testing.T) {
-	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		t.Logf("mockServer: %s %s", r.Method, r.URL.String())
-		w.Header().Set("Content-Type", "application/json")
-		if r.Method == "GET" && r.URL.String() == "/v4/clusters/cluster-id/" {
-			// cluster details before the patch
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(`{
-				"id": "cluster-id",
-				"name": "",
-				"api_endpoint": "",
-				"create_date": "2017-05-16T09:30:31.192170835Z",
-				"owner": "acmeorg",
-				"scaling": {
-					"max":3,
-					"min":3
-				},
-				"workers": [
-					{"memory": {"size_gb": 5}, "storage": {"size_gb": 50}, "cpu": {"cores": 2}, "labels": {"foo": "bar"}},
-					{"memory": {"size_gb": 5}, "storage": {"size_gb": 50}, "cpu": {"cores": 2}, "labels": {"foo": "bar"}},
-					{"memory": {"size_gb": 5}, "storage": {"size_gb": 50}, "cpu": {"cores": 2}, "labels": {"foo": "bar"}}
-				]
-			}`))
-		} else if r.Method == "PATCH" && r.URL.String() == "/v4/clusters/cluster-id/" {
-			// inspect PATCH request body
-			w.WriteHeader(http.StatusOK)
-			patchBytes, readErr := ioutil.ReadAll(r.Body)
-			if readErr != nil {
-				t.Error(readErr)
-			}
-			patch, parseErr := gabs.ParseJSON(patchBytes)
-			if parseErr != nil {
-				t.Error(parseErr)
-			}
-			if !patch.Exists("workers") {
-				t.Error("Patch request body does not contain 'workers' key.")
-			}
+	testCases := []struct {
+		numWorkersBefore  int
+		initialMinScaling int
+		desiredMinScaling int
+		workersMinSet     bool
+		initialMaxScaling int
+		desiredMaxScaling int
+		workersMaxSet     bool
+		numWorkersAfter   int
+	}{
+		{
+			numWorkersBefore:  3,
+			initialMinScaling: 3,
+			desiredMinScaling: 5,
+			workersMinSet:     true,
+			initialMaxScaling: 3,
+			desiredMaxScaling: 5,
+			workersMaxSet:     true,
+			numWorkersAfter:   5,
+		},
+	}
 
-			w.Write([]byte(`{
-				"id": "cluster-id",
-				"name": "",
-				"api_endpoint": "",
-				"create_date": "2017-05-16T09:30:31.192170835Z",
-				"owner": "acmeorg",
-				"workers": [
-					{"memory": {"size_gb": 5}, "storage": {"size_gb": 50}, "cpu": {"cores": 2}, "labels": {"foo": "bar"}},
-					{"memory": {"size_gb": 5}, "storage": {"size_gb": 50}, "cpu": {"cores": 2}, "labels": {"foo": "bar"}},
-					{"memory": {"size_gb": 5}, "storage": {"size_gb": 50}, "cpu": {"cores": 2}, "labels": {"foo": "bar"}},
-					{"memory": {"size_gb": 5}, "storage": {"size_gb": 50}, "cpu": {"cores": 2}, "labels": {"foo": "bar"}},
-					{"memory": {"size_gb": 5}, "storage": {"size_gb": 50}, "cpu": {"cores": 2}, "labels": {"foo": "bar"}}
-				]
-			}`))
-		} else if r.Method == "GET" && r.URL.String() == "/v4/clusters/cluster-id/status/" {
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(`{
+	for i, tc := range testCases {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+
+			mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				t.Logf("mockServer: %s %s", r.Method, r.URL.String())
+				w.Header().Set("Content-Type", "application/json")
+				if r.Method == "GET" && r.URL.String() == "/v4/clusters/cluster-id/" {
+					// cluster details before the patch
+					w.WriteHeader(http.StatusOK)
+					w.Write(generateClusterResponse(tc.numWorkersBefore, tc.initialMinScaling, tc.initialMaxScaling))
+				} else if r.Method == "PATCH" && r.URL.String() == "/v4/clusters/cluster-id/" {
+					// inspect PATCH request body
+					w.WriteHeader(http.StatusOK)
+					patchBytes, readErr := ioutil.ReadAll(r.Body)
+					if readErr != nil {
+						t.Error(readErr)
+					}
+					patch, parseErr := gabs.ParseJSON(patchBytes)
+					if parseErr != nil {
+						t.Error(parseErr)
+					}
+					if !patch.Exists("workers") {
+						t.Error("Patch request body does not contain 'workers' key.")
+					}
+
+					w.Write(generateClusterResponse(tc.numWorkersAfter, 0, 0))
+				} else if r.Method == "GET" && r.URL.String() == "/v4/clusters/cluster-id/status/" {
+					w.WriteHeader(http.StatusOK)
+					w.Write([]byte(`{
 				"cluster": {
 					"conditions": [
 						{
@@ -370,60 +370,97 @@ func TestScaleCluster(t *testing.T) {
 					]
 				}
 			}`))
-		} else {
-			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte(`{"code": "RESOURCE_NOT_FOUND", "message": "Could not find this."}`))
-		}
-	}))
-	defer mockServer.Close()
+				} else {
+					w.WriteHeader(http.StatusNotFound)
+					w.Write([]byte(`{"code": "RESOURCE_NOT_FOUND", "message": "Could not find this."}`))
+				}
+			}))
+			defer mockServer.Close()
 
-	// config
-	yamlText := `
+			// config
+			yamlText := `
 endpoints:
   ` + mockServer.URL + `:
     email: email@example.com
     token: some-token
     provider: aws
 selected_endpoint: ` + mockServer.URL
-	fs := afero.NewMemMapFs()
-	_, err := testutils.TempConfig(fs, yamlText)
-	if err != nil {
-		t.Error(err)
+			fs := afero.NewMemMapFs()
+			_, err := testutils.TempConfig(fs, yamlText)
+			if err != nil {
+				t.Error(err)
+			}
+
+			testArgs := Arguments{
+				APIEndpoint:       mockServer.URL,
+				ClusterNameOrID:   "cluster-id",
+				WorkersMax:        int64(tc.desiredMaxScaling),
+				WorkersMin:        int64(tc.desiredMinScaling),
+				UserProvidedToken: "my-token",
+				WorkersMinSet:     tc.workersMinSet,
+				WorkersMaxSet:     tc.workersMaxSet,
+			}
+
+			clientWrapper, err := client.NewWithConfig(testArgs.APIEndpoint, testArgs.UserProvidedToken)
+			if err != nil {
+				t.Errorf("Unexpected error '%s'", err)
+			}
+
+			err = verifyPreconditions(testArgs, clientWrapper)
+			if err != nil {
+				t.Error(err)
+			}
+
+			result, scaleErr := scaleCluster(testArgs)
+			if scaleErr != nil {
+				t.Error(scaleErr)
+			}
+
+			expectedResult := Result{
+				NumWorkersBefore: tc.numWorkersBefore,
+				ScalingMinBefore: tc.initialMinScaling,
+				ScalingMinAfter:  tc.desiredMinScaling,
+				ScalingMaxBefore: tc.initialMaxScaling,
+				ScalingMaxAfter:  tc.desiredMaxScaling,
+			}
+			if diff := cmp.Diff(expectedResult, *result); diff != "" {
+				t.Errorf("Case %d: Scaling result unequal. (-expected +got):\n%s", i, diff)
+			}
+		})
+	}
+}
+
+func generateClusterResponse(workerCount int, workersMin int, workersMax int) []byte {
+	c := models.V4ClusterDetailsResponse{
+		ID:          "cluster-id",
+		Name:        "",
+		APIEndpoint: "",
+		CreateDate:  "2017-05-16T09:30:31.192170835Z",
+		Owner:       "acmeorg",
+		Scaling: &models.V4ClusterDetailsResponseScaling{
+			Min: int64(workersMin),
+			Max: int64(workersMax),
+		},
 	}
 
-	testArgs := Arguments{
-		APIEndpoint:       mockServer.URL,
-		ClusterNameOrID:   "cluster-id",
-		WorkersMax:        int64(5),
-		WorkersMin:        int64(5),
-		UserProvidedToken: "my-token",
-		WorkersMinSet:     true,
-		WorkersMaxSet:     true,
+	for i := 0; i < workerCount; i++ {
+		c.Workers = append(c.Workers, &models.V4ClusterDetailsResponseWorkersItems{
+			Memory: &models.V4ClusterDetailsResponseWorkersItemsMemory{
+				SizeGb: 5,
+			},
+			Storage: &models.V4ClusterDetailsResponseWorkersItemsStorage{
+				SizeGb: 5,
+			},
+			CPU: &models.V4ClusterDetailsResponseWorkersItemsCPU{
+				Cores: 2,
+			},
+			Labels: map[string]string{
+				"foo": "bar",
+			},
+		})
 	}
 
-	clientWrapper, err := client.NewWithConfig(testArgs.APIEndpoint, testArgs.UserProvidedToken)
-	if err != nil {
-		t.Errorf("Unexpected error '%s'", err)
-	}
+	bytes, _ := json.Marshal(c)
 
-	err = verifyPreconditions(testArgs, clientWrapper)
-	if err != nil {
-		t.Error(err)
-	}
-
-	result, scaleErr := scaleCluster(testArgs)
-	if scaleErr != nil {
-		t.Error(scaleErr)
-	}
-
-	expectedResult := Result{
-		NumWorkersBefore: 3,
-		ScalingMinBefore: 3,
-		ScalingMinAfter:  5,
-		ScalingMaxBefore: 3,
-		ScalingMaxAfter:  5,
-	}
-	if diff := cmp.Diff(expectedResult, *result); diff != "" {
-		t.Errorf("Scaling result unequal. (-expected +got):\n%s", diff)
-	}
+	return bytes
 }
