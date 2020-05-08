b1e74d12108fa91c5b5d7ad1c5514f2f3d08e54f
Adrian Barsan <mnlk123@yahoo.com>
2020-05-08 09:39:12 +0200

    Add json output to the 'list nodepools' command (#545)
    * Add json output argument
    
    * Add validation error printing/exit logic same as listing clusters
    
    * Revert "Add validation error printing/exit logic same as listing clusters"
    
    This reverts commit cb32e076
    
    * Refactor printing to separate method
    
    * Pre-allocate table rows
    
    * Remove the need of the result row data structure
    
    * Clean up formatting on the instancetypes variable
    
    * Add json format to output generation
    
    * Actually exit if there's a validation error
    
    * Actually exit if there's a validation error
    
    * Refactor tests to support multiple commands and add multiple test cases, testing different outputs
    
    * Update commands/list/nodepools/command.go
    
    Co-authored-by: Oliver Ponder <oliverponder@giantswarm.io>
    
    Co-authored-by: Oliver Ponder <oliverponder@giantswarm.io>

--- commands/list/nodepools/command_test.go
+++ commands/list/nodepools/command_test.go
@@ -3,30 +3,89 @@ package nodepools
 import (
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 
 	"github.com/giantswarm/gscliauth/config"
+	"github.com/google/go-cmp/cmp"
 	"github.com/spf13/afero"
 
 	"github.com/giantswarm/gsctl/testutils"
 )
 
 func Test_ListNodePools(t *testing.T) {
-	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		if r.Method == "GET" {
-			switch uri := r.URL.Path; uri {
-			case "/v5/clusters/cluster-id/nodepools/":
-				w.WriteHeader(http.StatusOK)
-				w.Write([]byte(`[
-					{"id": "a7rc4", "name": "Batch number crunching", "availability_zones": ["eu-west-1d"], "scaling": {"min": 2, "max": 5}, "node_spec": {"aws": {"instance_type": "p3.8xlarge"}, "volume_sizes_gb": {"docker": 100, "kubelet": 100}}, "status": {"nodes": 4, "nodes_ready": 4}},
-					{"id": "6feel", "name": "Application servers", "availability_zones": ["eu-west-1a", "eu-west-1b", "eu-west-1c"], "scaling": {"min": 3, "max": 15}, "node_spec": {"aws": {"instance_type": "p3.2xlarge"}, "volume_sizes_gb": {"docker": 100, "kubelet": 100}}, "status": {"nodes": 10, "nodes_ready": 9}},
-					{"id": "a6bf4", "name": "New node pool", "availability_zones": ["eu-west-1c"], "scaling": {"min": 3, "max": 3}, "node_spec": {"aws": {"instance_type": "m5.2xlarge"}, "volume_sizes_gb": {"docker": 100, "kubelet": 100}}, "status": {"nodes": 0, "nodes_ready": 0}}
-				]`))
+	testCases := []struct {
+		npResponse   string
+		outputFormat string
+		output       string
+	}{
+		{
+			npResponse: `[
+                {"id": "a7rc4", "name": "Batch number crunching", "availability_zones": ["eu-west-1d"], "scaling": {"min": 2, "max": 5}, "node_spec": {"aws": {"instance_type": "p3.8xlarge", "instance_distribution": {"on_demand_base_capacity": 0, "on_demand_percentage_above_base_capacity": 0}}, "volume_sizes_gb": {"docker": 100, "kubelet": 100}}, "status": {"nodes": 4, "nodes_ready": 4}},
+                {"id": "6feel", "name": "Application servers", "availability_zones": ["eu-west-1a", "eu-west-1b", "eu-west-1c"], "scaling": {"min": 3, "max": 15}, "node_spec": {"aws": {"instance_type": "p3.2xlarge", "instance_distribution": {"on_demand_base_capacity": 0, "on_demand_percentage_above_base_capacity": 0}}, "volume_sizes_gb": {"docker": 100, "kubelet": 100}}, "status": {"nodes": 10, "nodes_ready": 9}},
+                {"id": "a6bf4", "name": "New node pool", "availability_zones": ["eu-west-1c"], "scaling": {"min": 3, "max": 3}, "node_spec": {"aws": {"instance_type": "m5.2xlarge", "instance_distribution": {"on_demand_base_capacity": 0, "on_demand_percentage_above_base_capacity": 0}}, "volume_sizes_gb": {"docker": 100, "kubelet": 100}}, "status": {"nodes": 0, "nodes_ready": 0}}
+            ]`,
+			outputFormat: "table",
+			output: `ID     NAME                    AZ     INSTANCE TYPE  ALIKE  ON-DEMAND BASE  SPOT PERCENTAGE  NODES MIN/MAX  NODES DESIRED  NODES READY  SPOT INSTANCES  CPUS  RAM (GB)
+6feel  Application servers     A,B,C  p3.2xlarge     false               0              100           3/15             10            9               0    72     549.0
+a6bf4  New node pool           C      m5.2xlarge     false               0              100            3/3              0            0               0     0       0.0
+a7rc4  Batch number crunching  D      p3.8xlarge     false               0              100            2/5              4            4               0   128     976.0`,
+		},
+		{
+			npResponse: `[
+                {"id": "a6bf4", "name": "New node pool", "availability_zones": ["eu-west-1c"], "scaling": {"min": 3, "max": 3}, "node_spec": {"aws": {"instance_type": "m5.2xlarge", "instance_distribution": {"on_demand_base_capacity": 0, "on_demand_percentage_above_base_capacity": 0}}, "volume_sizes_gb": {"docker": 100, "kubelet": 100}}, "status": {"nodes": 0, "nodes_ready": 0}}
+            ]`,
+			outputFormat: "table",
+			output: `ID     NAME           AZ  INSTANCE TYPE  ALIKE  ON-DEMAND BASE  SPOT PERCENTAGE  NODES MIN/MAX  NODES DESIRED  NODES READY  SPOT INSTANCES  CPUS  RAM (GB)
+a6bf4  New node pool  C   m5.2xlarge     false               0              100            3/3              0            0               0     0       0.0`,
+		},
+		{
+			npResponse: `[
+                {"id": "a6bf4", "name": "New node pool", "availability_zones": ["eu-west-1c"], "scaling": {"min": 3, "max": 3}, "node_spec": {"aws": {"instance_type": "m5.2xlarge", "instance_distribution": {"on_demand_base_capacity": 0, "on_demand_percentage_above_base_capacity": 0}}, "volume_sizes_gb": {"docker": 100, "kubelet": 100}}, "status": {"nodes": 0, "nodes_ready": 0}}
+            ]`,
+			outputFormat: "json",
+			output: `[
+  {
+    "availability_zones": [
+      "eu-west-1c"
+    ],
+    "id": "a6bf4",
+    "name": "New node pool",
+    "node_spec": {
+      "aws": {
+        "instance_distribution": {},
+        "instance_type": "m5.2xlarge"
+      },
+      "volume_sizes_gb": {
+        "docker": 100,
+        "kubelet": 100
+      }
+    },
+    "scaling": {
+      "max": 3,
+      "min": 3
+    },
+    "status": {
+      "instance_types": null
+    }
+  }
+]`,
+		},
+	}
 
-			case "/v4/clusters/":
-				w.WriteHeader(http.StatusOK)
-				w.Write([]byte(`[
+	for i, tc := range testCases {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "application/json")
+				if r.Method == "GET" {
+					switch uri := r.URL.Path; uri {
+					case "/v5/clusters/cluster-id/nodepools/":
+						w.WriteHeader(http.StatusOK)
+						w.Write([]byte(tc.npResponse))
+
+					case "/v4/clusters/":
+						w.WriteHeader(http.StatusOK)
+						w.Write([]byte(`[
 					{
 						"id": "cluster-id",
 						"name": "Name of the cluster",
@@ -34,45 +93,45 @@ func Test_ListNodePools(t *testing.T) {
 					}
 				]`))
 
-			default:
-				t.Errorf("Unsupported route %s called in mock server", r.URL.Path)
-				w.WriteHeader(http.StatusNotFound)
-				w.Write([]byte(`{"code": "RESOURCE_NOT_FOUND", "message": "Status for this cluster is not yet available."}`))
+					default:
+						t.Errorf("Case %d: Unsupported route %s called in mock server", i, r.URL.Path)
+						w.WriteHeader(http.StatusNotFound)
+						w.Write([]byte(`{"code": "RESOURCE_NOT_FOUND", "message": "Status for this cluster is not yet available."}`))
+					}
+				}
+			}))
+			defer mockServer.Close()
+
+			// temp config
+			fs := afero.NewMemMapFs()
+			configDir := testutils.TempDir(fs)
+			config.Initialize(fs, configDir)
+
+			args := Arguments{
+				clusterNameOrID: "cluster-id",
+				apiEndpoint:     mockServer.URL,
+				authToken:       "my-token",
+				outputFormat:    tc.outputFormat,
 			}
-		}
-	}))
-	defer mockServer.Close()
 
-	// temp config
-	fs := afero.NewMemMapFs()
-	configDir := testutils.TempDir(fs)
-	config.Initialize(fs, configDir)
+			err := verifyPreconditions(args, []string{args.clusterNameOrID})
+			if err != nil {
+				t.Errorf("Case %d: %s", i, err)
+			}
 
-	args := Arguments{
-		clusterNameOrID: "cluster-id",
-		apiEndpoint:     mockServer.URL,
-		authToken:       "my-token",
-	}
+			results, err := fetchNodePools(args)
+			if err != nil {
+				t.Errorf("Case %d: %s", i, err)
+			}
 
-	err := verifyPreconditions(args, []string{args.clusterNameOrID})
-	if err != nil {
-		t.Error(err)
-	}
+			output, err := getOutput(results, args.outputFormat)
+			if err != nil {
+				t.Errorf("Case %d: %s", i, err)
+			}
 
-	results, err := fetchNodePools(args)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if len(results) != 3 {
-		t.Errorf("Expected length 3, got %d", len(results))
-	}
-
-	if results[0].nodePool.ID != "6feel" {
-		t.Errorf("Expected NP ID 6feel in the front, got %s", results[0].nodePool.ID)
-	}
-
-	if results[2].sumCPUs != 128 {
-		t.Errorf("Expected 128 CPUs in the third row, got %d", results[2].sumCPUs)
+			if diff := cmp.Diff(tc.output, output); diff != "" {
+				t.Errorf("Case %d - Command output is incorrect. (-expected +got):\n%s", i, diff)
+			}
+		})
 	}
 }
