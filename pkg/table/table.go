7630adf4b7c9a28660ceee222dfe1a42b09aa8e2
axbarsan <mnlk123@yahoo.com>
2020-05-14 16:15:55 +0200

    Move .String() method to end of file

--- pkg/table/table.go
+++ pkg/table/table.go
@@ -34,28 +34,6 @@ func (t *Table) SetRows(r [][]string) {
 	t.rows = r[:][:]
 }
 
-func (t *Table) String() string {
-	rows := make([]string, 0, len(t.rows)+1)
-
-	{
-		columns := make([]string, 0, len(t.columns))
-		for _, col := range t.columns {
-			columns = append(columns, col.GetHeader())
-		}
-		rows = append(rows, strings.Join(columns, "|"))
-	}
-
-	{
-		for _, row := range t.rows {
-			rows = append(rows, strings.Join(row, "|"))
-		}
-	}
-
-	formattedTable := columnize.Format(rows, t.columnizeConfig)
-
-	return formattedTable
-}
-
 func (t *Table) SortByColumnName(n string, direction string) error {
 	var (
 		colIndex int
@@ -86,4 +64,26 @@ func (t *Table) SortByColumnName(n string, direction string) error {
 	})
 
 	return nil
+}
+
+func (t *Table) String() string {
+	rows := make([]string, 0, len(t.rows)+1)
+
+	{
+		columns := make([]string, 0, len(t.columns))
+		for _, col := range t.columns {
+			columns = append(columns, col.GetHeader())
+		}
+		rows = append(rows, strings.Join(columns, "|"))
+	}
+
+	{
+		for _, row := range t.rows {
+			rows = append(rows, strings.Join(row, "|"))
+		}
+	}
+
+	formattedTable := columnize.Format(rows, t.columnizeConfig)
+
+	return formattedTable
 }
