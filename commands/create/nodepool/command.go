4c62b4c0ade210a2ec376214445db8fc78405e1a
axbarsan <mnlk123@yahoo.com>
2020-08-04 16:56:15 +0200

    Update default node count to 3

--- commands/create/nodepool/command.go
+++ commands/create/nodepool/command.go
@@ -51,7 +51,7 @@ follows:
 - Availability zones: the node pool will use 1 zone selected randomly.
 - Instance type (AWS) / VM Size (Azure): the default machine type of the installation will be
   used. Check 'gsctl info' to find out what that is.
-- Scaling settings: on AWS, the minimum will be 3 and maximum 10 nodes, and on Azure, the node count will be 10
+- Scaling settings: on AWS, the minimum will be 3 and maximum 10 nodes, and on Azure, the node count will be 3
 
 Examples:
 
