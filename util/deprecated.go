f3833b382f266df251d46c4acd798974139a8dfd
Sandy Kuo <kuo.sandys@gmail.com>
2022-01-31 18:07:43 +0100

    Move provider checking logic to 'GetDeprecatedNotice' utility function and add tests

--- util/deprecated.go
+++ util/deprecated.go
@@ -1,10 +1,19 @@
 package util
 
-import "fmt"
+import (
+	"fmt"
 
-func Deprecated(gsctlCmd string, kubectlgsCmd string, docsURL string) string {
+	p "github.com/giantswarm/gsctl/pkg/provider"
+)
+
+func GetDeprecatedNotice(provider, gsctlCmd, kubectlgsCmd, docsURL string) string {
+	if provider == p.KVM {
+		return ""
+	}
+
 	return fmt.Sprintf(`Command "%s" is deprecated, gsctl is being phased out in favor of our 'kubectl gs' plugin.
 We recommend you familiarize yourself with the "kubectl gs %s" command as a replacement for this.
 For more details see: %s
+
 `, gsctlCmd, kubectlgsCmd, docsURL)
 }
