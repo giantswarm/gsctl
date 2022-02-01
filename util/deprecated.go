c5220e4029188699b1b03c58ede063e131dea003
Sandy Kuo <kuo.sandys@gmail.com>
2022-02-01 10:00:55 +0100

    Highlight command literals with yellow and remove double quotes

--- util/deprecated.go
+++ util/deprecated.go
@@ -3,6 +3,7 @@ package util
 import (
 	"fmt"
 
+	"github.com/fatih/color"
 	p "github.com/giantswarm/gsctl/pkg/provider"
 )
 
@@ -11,9 +12,9 @@ func GetDeprecatedNotice(provider, gsctlCmd, kubectlgsCmd, docsURL string) strin
 		return ""
 	}
 
-	return fmt.Sprintf(`Command "%s" is deprecated, gsctl is being phased out in favor of our 'kubectl gs' plugin.
-We recommend you familiarize yourself with the "kubectl gs %s" command as a replacement for this.
+	return fmt.Sprintf(`Command %s is deprecated, gsctl is being phased out in favor of our kubectl-gs plugin.
+We recommend you familiarize yourself with the %s command as a replacement for this.
 For more details see: %s
 
-`, gsctlCmd, kubectlgsCmd, docsURL)
+`, color.YellowString(gsctlCmd), color.YellowString("kubectl gs "+kubectlgsCmd), docsURL)
 }
